9e05f587228625bb2224a92df4b1703bf6acc465
Teja Juthuga <tejajuthuga@gmail.com>
2024-10-30 06:08:06 +0530

    Developed Strick Through and Delete Feature in Todo App

--- MERN_StackDelevelopment/JS/23.10.2024/To-do's Application/script.js
+++ MERN_StackDelevelopment/JS/23.10.2024/To-do's Application/script.js
@@ -15,19 +15,41 @@ let tasks = [
 
 let mainTodosContainer = document.getElementById("main-todos-container");
 
+function onClickStatusChange(labelId, checkboxId){
+    let labelEle =  document.getElementById(labelId);
+    labelEle.classList.toggle("status-change")
+}
+
+function deleteTask(taskId){
+    let taskCard = document.getElementById(taskId);
+    mainTodosContainer.removeChild(taskCard);
+}
+
 function createTasks(eachTask) {
+    let checkboxId = "checkbox" + eachTask.id;
+    let labelId = "label" + eachTask.id;
+    let taskId = "task" + eachTask.id;
+    
+
     let divEle = document.createElement("div");
     divEle.setAttribute("class", "todo-card");
+    divEle.id = taskId;
 
     let inputEle = document.createElement("input");
     inputEle.setAttribute("type", "checkbox");
     inputEle.setAttribute("class", "todo-checkbox");
+    inputEle.id = checkboxId;
+    inputEle.onclick = function() {
+        onClickStatusChange(labelId,checkboxId)
+    }
     divEle.appendChild(inputEle);
 
-    let pEle = document.createElement("p");
-    pEle.setAttribute("class", "todo-task");
-    pEle.innerText = eachTask["task"];
-    divEle.appendChild(pEle);
+    let labelEle = document.createElement("label");
+    labelEle.setAttribute("class", "todo-task");
+    labelEle.setAttribute("for", checkboxId);
+    labelEle.id = labelId;
+    labelEle.innerText = eachTask["task"];
+    divEle.appendChild(labelEle);
 
     let featuresDiv = document.createElement("div");
     featuresDiv.setAttribute("class", "to-do-modification-features");
@@ -40,13 +62,17 @@ function createTasks(eachTask) {
     let delEle = document.createElement("i");
     delEle.setAttribute("class", "fa-solid fa-trash");
     featuresDiv.appendChild(delEle);
+    delEle.onclick = function(){
+        deleteTask(taskId);
+    }
+
 
     mainTodosContainer.appendChild(divEle);
 }
 
 function addTask(){
     let task = document.getElementById("todo-task");
-    console.log(task.value);    
+    console.log(task.value);
 }
 
 for (let task of tasks) {
