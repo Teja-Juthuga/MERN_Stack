3a42923a92ba7ae5e734131ad4bf0a4c3d10b7f5
Teja Juthuga <tejajuthuga@gmail.com>
2024-11-06 06:27:22 +0530

    Developed feature, Saved tasks into local storage

--- MERN_StackDelevelopment/JS/23.10.2024/To-do's Application/script.js
+++ MERN_StackDelevelopment/JS/23.10.2024/To-do's Application/script.js
@@ -1,20 +1,23 @@
-let tasks = [
-    {
-        id: 1,
-        task: "Learn Javascript",
-    },
-    {
-        id: 2,
-        task: "Learn React",
-    },
-    {
-        id: 3,
-        task: "Learn Sql",
-    },
-];
+function getTasksFromLocalStorage(){
+    let data = localStorage.getItem("todoTasks");
+    let parsedData = JSON.parse(data);
+    return parsedData;
+}
+
+let tasks = getTasksFromLocalStorage();
 
 let mainTodosContainer = document.getElementById("main-todos-container");
-let tasksCount = tasks.length
+let tasksCount = tasks.length;
+let saveBtn = document.getElementById("save-task");
+
+function saveTask(){
+    localStorage.setItem("todoTasks" , JSON.stringify(tasks));
+    console.log("saved!");
+}
+
+saveBtn.onclick = function(){
+    saveTask()
+}
 
 function onClickStatusChange(labelId, checkboxId){
     let labelEle =  document.getElementById(labelId);
@@ -24,6 +27,17 @@ function onClickStatusChange(labelId, checkboxId){
 function deleteTask(taskId){
     let taskCard = document.getElementById(taskId);
     mainTodosContainer.removeChild(taskCard);
+    let requiredIndex = tasks.findIndex(function(eachTask){
+        requiredTaskId = "task"+eachTask.id
+        if (taskId === requiredTaskId){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+    tasks.splice(requiredIndex,1);
+
 }
 
 function createTasks(eachTask) {
@@ -81,7 +95,9 @@ function addTask(){
         let newTask = {
             id: tasksCount + 1,
             task: task.value,
+            isTaskCompleted : false
         }
+        tasks.push(newTask);
         createTasks(newTask)
         task.value = ""
     }
