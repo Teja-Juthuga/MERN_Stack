d902fad784d02243f7b0c808faa4c2ba036a6335
Teja Juthuga <tejajuthuga@gmail.com>
2024-11-19 07:17:49 +0530

    Practiced Input elements

--- MERN_StackDelevelopment/JS/23.10.2024/To-do's Application/script.js
+++ MERN_StackDelevelopment/JS/23.10.2024/To-do's Application/script.js
@@ -1,4 +1,6 @@
-function getTasksFromLocalStorage(){
+let inputEle = document.getElementById("todo-task");
+
+function getTasksFromLocalStorage() {
     let data = localStorage.getItem("todoTasks");
     let parsedData = JSON.parse(data);
     return parsedData;
@@ -10,41 +12,73 @@ let mainTodosContainer = document.getElementById("main-todos-container");
 let tasksCount = tasks.length;
 let saveBtn = document.getElementById("save-task");
 
-function saveTask(){
-    localStorage.setItem("todoTasks" , JSON.stringify(tasks));
-    console.log("saved!");
+function saveTask() {
+    localStorage.setItem("todoTasks", JSON.stringify(tasks));
 }
 
-saveBtn.onclick = function(){
-    saveTask()
+saveBtn.onclick = function () {
+    saveTask();
+};
+
+function onClickStatusChange(labelId, checkboxId, taskId) {
+    let labelEle = document.getElementById(labelId);
+    labelEle.classList.toggle("status-change");
+
+    let checkboxElement = document.getElementById(checkboxId);
+    
+
+    let requiredIndex = tasks.findIndex(function (eachTask) {
+        let eachTodoId = "task" + eachTask.id;
+
+        if (eachTodoId === taskId) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    let task = tasks[requiredIndex];
+
+    if (task.isTaskCompleted === true) {
+        task.isTaskCompleted = false;
+    } else {
+        task.isTaskCompleted = true;
+    }
 }
 
-function onClickStatusChange(labelId, checkboxId){
-    let labelEle =  document.getElementById(labelId);
-    labelEle.classList.toggle("status-change")
-}
-
-function deleteTask(taskId){
+function deleteTask(taskId) {
     let taskCard = document.getElementById(taskId);
     mainTodosContainer.removeChild(taskCard);
+    let requiredIndex = tasks.findIndex(function (eachTask) {
+        requiredTaskId = "task" + eachTask.id;
+        if (taskId === requiredTaskId) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    tasks.splice(requiredIndex, 1);
+}
+/*
+function editTask(taskId){
     let requiredIndex = tasks.findIndex(function(eachTask){
         requiredTaskId = "task"+eachTask.id
         if (taskId === requiredTaskId){
             return true;
         }
-        else{
+        else {
             return false;
         }
     })
-    tasks.splice(requiredIndex,1);
 
+    inputEle.value = tasks[requiredIndex]['task'];   
 }
+*/
 
 function createTasks(eachTask) {
     let checkboxId = "checkbox" + eachTask.id;
     let labelId = "label" + eachTask.id;
     let taskId = "task" + eachTask.id;
-    
 
     let divEle = document.createElement("div");
     divEle.setAttribute("class", "todo-card");
@@ -54,9 +88,10 @@ function createTasks(eachTask) {
     inputEle.setAttribute("type", "checkbox");
     inputEle.setAttribute("class", "todo-checkbox");
     inputEle.id = checkboxId;
-    inputEle.onclick = function() {
-        onClickStatusChange(labelId,checkboxId)
-    }
+    inputEle.checked = tasks.isTaskCompleted;  
+    inputEle.onclick = function () {
+        onClickStatusChange(labelId, checkboxId, taskId);
+    };
     divEle.appendChild(inputEle);
 
     let labelEle = document.createElement("label");
@@ -69,40 +104,42 @@ function createTasks(eachTask) {
     let featuresDiv = document.createElement("div");
     featuresDiv.setAttribute("class", "to-do-modification-features");
     divEle.appendChild(featuresDiv);
-
+    /*
     let editEle = document.createElement("i");
     editEle.setAttribute("class", "fa-solid fa-pen");
+    editEle.onclick = function (){
+        editTask(taskId)
+    }
     featuresDiv.appendChild(editEle);
+    */
 
     let delEle = document.createElement("i");
     delEle.setAttribute("class", "fa-solid fa-trash");
     featuresDiv.appendChild(delEle);
-    delEle.onclick = function(){
+    delEle.onclick = function () {
         deleteTask(taskId);
-    }
-
+    };
 
     mainTodosContainer.appendChild(divEle);
 }
 
-function addTask(){
-    let task = document.getElementById("todo-task");
-    
-    if (task.value === ""){
+function addTask() {
+    // let task = document.getElementById("todo-task");
+
+    if (inputEle.value === "") {
         alert("Please Enter Valid Input!");
-    }
-    else {
+    } else {
         let newTask = {
-            id: tasksCount + 1,
-            task: task.value,
-            isTaskCompleted : false
-        }
+            id: Math.random() * 100,
+            task: inputEle.value,
+            isTaskCompleted: false,
+        };
         tasks.push(newTask);
-        createTasks(newTask)
-        task.value = ""
+        createTasks(newTask);
+        inputEle.value = "";
     }
 }
 
 for (let task of tasks) {
-    createTasks(task)
+    createTasks(task);
 }
